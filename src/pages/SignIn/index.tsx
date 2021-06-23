70109a9ce3869b555219edebc9ad2332887bd9e8
Amanda Silva <amanda-ms@hotmail.com>
2021-06-23 14:35:06 -0300

    feat: finish signIn screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { View, Text, Image, StatusBar } from "react-native";
+
 import { styles } from "./style";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from "../../assets/illustration.png";
 
 export function SignIn() {
-  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Hello World, NLW Together</Text>
-      <TextInput style={styles.input} onChangeText={setText} />
-      <Text>Você digitou: {text}</Text>
+      <StatusBar barStyle="light-content" />
+      <Image source={IllustrationImg} style={styles.img} resizeMode="stretch" />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se{"\n"}e organize suas {"\n"}
+          jogatinas
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{"\n"}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entrar com o Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
